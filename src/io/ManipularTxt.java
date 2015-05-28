d8c4f5d5a2a7784a3dba980321cdc7daa029dcf3
Valmar Júnior <valmar.junior1@gmail.com>
2015-05-28 16:08:14 -0300

    leitura do TXT terminado

--- src/io/ManipularTxt.java
+++ src/io/ManipularTxt.java
@@ -16,29 +16,39 @@ public class ManipularTxt {
 		FileReader fr = new FileReader(caminhoArquivo);
 		BufferedReader br = new BufferedReader(fr);
 		String line;
-		int tamanhoMatriz = Integer.parseInt(br.readLine());
+		int quantidadeVertices = Integer.parseInt(br.readLine());
 		// System.out.println(tamanhoMatriz);
-		int[][] matriz = new int[tamanhoMatriz][tamanhoMatriz];
+		int[][] matriz = new int[quantidadeVertices][quantidadeVertices];
 
 		while ((line = br.readLine()) != null) {
 			String[] values = line.split(" ");
 			for (String string : values) {
-				int stringInt = Integer.parseInt(string);
-				matriz[x][y] = stringInt;
-				y = y + 1;
+				if (string.equalsIgnoreCase("inf")) {
+					// Para caso Grafo ponderado.
+					matriz[x][y] = Integer.MAX_VALUE;
+					y = y + 1;
+				} else {
+					int stringInt = Integer.parseInt(string);
+					matriz[x][y] = stringInt;
+					y = y + 1;
+				}
+
 				// matrizAdjacencia[x][y] = stringInt;
 			}
 			x = x + 1;
 			y = 0;
 		}
+
 		br.close();
 		fr.close();
 
-		/*
-		 * for (int i = 0; i < matriz.length; i++) { for (int j = 0; j <
-		 * matriz.length; j++) { System.out.print(" " + matriz[i][j]); }
-		 * System.out.println(); }
-		 */
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz.length; j++) {
+				System.out.print(" " + matriz[i][j]);
+			}
+			System.out.println();
+		}
+
 	}
 
 }
