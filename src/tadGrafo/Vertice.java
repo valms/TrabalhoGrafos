aa3c02984c26212e152a86d37453dfc1227cd4d5
Valmar Júnior <valmar.junior1@gmail.com>
2015-06-10 10:18:19 -0300

    .

--- src/tadGrafo/Vertice.java
+++ src/tadGrafo/Vertice.java
@@ -2,6 +2,22 @@ package tadGrafo;
 
 public class Vertice {
 	private int cor;
-	
+	private int adjacentes[];
+
+	public int getCor() {
+		return cor;
+	}
+
+	public int[] getAdjacentes() {
+		return adjacentes;
+	}
+
+	public void setCor(int cor) {
+		this.cor = cor;
+	}
+
+	public void setAdjacentes(int[] adjacentes) {
+		this.adjacentes = adjacentes;
+	}
 
 }
